1a864af754894a4ab51b29618e9465a38d76cf1d
BAlexM <bartok1717@gmail.com>
2024-01-31 13:45:32 +0100

    3. Feladat (lista kiiratása [nem működik])

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 const numberInput = document.getElementById('numberInput')
 const numberList = document.getElementById('numberList')
+numbers = [];
+
+function addNumber() {
+    const inputValue = numberInput.inputValue;
+
+    numbers.push(Number(inputValue));
+    updateList();
+    numberInput.value='';
+
+
+    function updateList(){
+        const listItems = numbers.map(number => `<li>$number</li>`);
+        numberList.innerHTML = listItems.join('');
+        alert(`Számok: ${numbers.join(', ')}`)
+        
+
+    }
+
+}
